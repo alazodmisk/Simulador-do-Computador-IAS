b3a9e2878b90f6009bd082cc37671a929c795aeb
Guilherme H. V. Pichitelli Vitor <pichitellig@gmail.com>
2026-05-27 21:24:56 -0300

    ciclo

---
+++ ciclo.c
@@ -0,0 +1,25 @@
+#include "ciclo.h"
+
+void buscaInstrucao(){
+    /*
+    Fazer o codigo
+    */
+}
+
+void buscaOperando(){
+    /*
+    Fazer o codigo
+    */
+}
+
+void executaInstrucao(){
+    /*
+    Fazer o codigo
+    */
+}
+
+void escreveResultado(){
+    /*
+    Fazer o codigo
+    */
+}
---
+++ ciclo.h
@@ -0,0 +1,9 @@
+#ifndef CICLO_H
+#define CICLO_H
+
+void buscaInstrucao();
+void buscaOperando();
+void executaInstrucao();
+void escreveResultado();
+
+#endif
